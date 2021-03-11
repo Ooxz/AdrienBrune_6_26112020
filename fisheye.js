3b67e604fe06ba46c0ae3e62c433af0185cec3b2
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-03-11 21:08:40 +0100

    update js

--- fisheye.js
+++ fisheye.js
@@ -1,31 +1,4 @@
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-//fonctionnalitées de la croix pour fermer le formulaire
-document.getElementById('closeButton').addEventListener('click', (event) => {
-  event.preventDefault();
-  modalbg.style.display = 'none';
-  });
-
-  //clicking outside of the modalbg will close it
-  window.onclick = function(event) {
-    if (event.target == modalbg) {
-      modalbg.style.display = "none";
-    }
-  }
-
-
-/*adJson () 
+//adJson () 
     // http://localhost:8080
     fetch('https://ooxz.github.io/AdrienBrune_6_26112020/photographers.json')
     .then(response => {
@@ -35,12 +8,20 @@ document.getElementById('closeButton').addEventListener('click', (event) => {
         return response.json();
     })
     .then(json => {
-        this.users = json;
-        //console.log(this.users);
+        let users = json;
+        users.photographers.forEach(pic => {
+          document.body.innerHTML += `<h1>${pic.portrait}</h1`
+          document.body.innerHTML += `<h1>${pic.name}</h1`
+          document.body.innerHTML += `<h1>${pic.city}</h1`
+          document.body.innerHTML += `<h1>${pic.country}</h1`
+          document.body.innerHTML += `<h1>${pic.tagline}</h1`
+          document.body.innerHTML += `<h1>${pic.price}</h1`
+        })
+        console.log(users);
     })
     .catch(function () {
         this.dataError = true;
-    })*/
+    })
  
     /*var requestURL = 'https://ooxz.github.io/AdrienBrune_6_26112020/photographers.json';
     var request = new XMLHttpRequest();
---
+++ photo.js
@@ -0,0 +1,25 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+//fonctionnalitées de la croix pour fermer le formulaire
+document.getElementById('closeButton').addEventListener('click', (event) => {
+  event.preventDefault();
+  modalbg.style.display = 'none';
+  });
+
+  //clicking outside of the modalbg will close it
+  window.onclick = function(event) {
+    if (event.target == modalbg) {
+      modalbg.style.display = "none";
+    }
+  }
