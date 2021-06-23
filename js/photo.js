f9ebddb78ded9bd27245c121aeb82349545ce13a
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-06-23 16:25:19 +0200

    update lightbox

--- js/photo.js
+++ js/photo.js
@@ -158,9 +158,22 @@ function launchModal() {
  * launch lightbox
  */
 function launchLightbox(elt) {
-  console.log(elt.nodeName);
-  console.log(elt.dataset.id);
   console.log("mediasFromData", mediasFromData);
+  let previousId;
+  let nextId;
+  let mediaIndex = mediasFromData.findIndex(media => media.id == elt.dataset.id);
+  if(mediaIndex == 0){
+    previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
+    nextId = mediasFromData[1].id;
+  }else if(mediaIndex == (mediasFromData.length - 1)){
+    previousId = mediasFromData[mediasFromData.length - 2].id;
+    nextId = mediasFromData[0].id;
+  }else{
+    previousId = mediasFromData[mediaIndex - 1].id;
+    nextId = mediasFromData[mediaIndex + 1].id;
+  }
+  document.querySelector(".lightbox__prev").dataset.id = previousId;
+  console.log(elt.dataset.id, previousId, nextId);
   // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
   lightboxbg.style.display = "block";
   lightboxCnt.style.display = "block";
@@ -179,7 +192,7 @@ function imageLightboxListener() {
 
 // Navigation prochaine et précédente (souris et clavier)
 next.addEventListener('click', () => goToNextSlide())
-prev.addEventListener('click', () => goToPreviousSlide())
+document.addEventListener('click', () => goToPreviousSlide())
 
 document.addEventListener('keydown', (e) => {
   const keyCode = e.key
@@ -206,18 +219,33 @@ function goToNextSlide() {
   }
 }
 function goToPreviousSlide() {
-  let items = document.querySelectorAll('.lightboxItem')
-  let total = items.length - 1
-  if (position - 1 >= 0) {
-    position -= 1
-    const currentItem = document.querySelector(`.item-${position}`)
-    const lastItem = document.querySelector(`.item-${position + 1}`)
-    setNodeAttributes(lastItem, currentItem)
-  } else {
-    const lastItem = document.querySelector(`.item-${position}`)
-    position = total
-    const currentItem = document.querySelector(`.item-${position}`)
-    setNodeAttributes(lastItem, currentItem)
+  const photographerFolder = getPhotographerFolder(photographer.name);
+  let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
+  let id =  document.querySelector(".lightbox__prev").dataset.id;
+  console.log("mediasFromData", mediasFromData);
+  let previousId;
+  let nextId;
+  let mediaIndex = mediasFromData.findIndex(media => media.id == id);
+  if(mediaIndex == 0){
+    previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
+    nextId = mediasFromData[1].id;
+  }else if(mediaIndex == (mediasFromData.length - 1)){
+    previousId = mediasFromData[mediasFromData.length - 2].id;
+    nextId = mediasFromData[0].id;
+  }else{
+    previousId = mediasFromData[mediaIndex - 1].id;
+    nextId = mediasFromData[mediaIndex + 1].id;
+  }
+  document.querySelector(".lightbox__prev").dataset.id = previousId;
+  console.log(id, previousId, nextId);
+  // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
+  lightboxbg.style.display = "block";
+  lightboxCnt.style.display = "block";
+  // lightboxBtn.style.display = "flex";
+  if(true){
+    document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${mediasFromData[mediaIndex].image}" style="width:100%">`;
+  }else{
+    document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${mediasFromData[mediaIndex].video}" style="width:100%" type="video/mp4"></video>`;
   }
 }
 
