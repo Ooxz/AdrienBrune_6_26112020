84ebcd4320a1d7dc3d75e86651172245a404fbdb
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-06-30 01:20:14 +0200

    update lightbox

--- js/photo.js
+++ js/photo.js
@@ -168,33 +168,22 @@ function launchModal() {
  */
 function launchLightbox(elt) {
   console.log("mediasFromData", mediasFromData);
-  // const photographerTitle = document.querySelector('.infos__title')
-  let previousId;
-  let nextId;
-  let mediaIndex = mediasFromData.findIndex(media => media.id == elt.dataset.id);
-  if(mediaIndex == 0){
-    previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
-    nextId = mediasFromData[1].id;
-  }else if(mediaIndex == (mediasFromData.length - 1)){
-    previousId = mediasFromData[mediasFromData.length - 2].id;
-    nextId = mediasFromData[0].id;
-  }else{
-    previousId = mediasFromData[mediaIndex - 1].id;
-    nextId = mediasFromData[mediaIndex + 1].id;
-  }
-  document.querySelector(".lightbox__prev").dataset.id = previousId;
-  document.querySelector(".lightbox__next").dataset.id = nextId;
-  console.log(elt.dataset.id, previousId, nextId);
+  const photographerFolder = getPhotographerFolder(photographer.name);
+  let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
+  let mediasToDisplay = pickUpMediasToDisplay(elt.dataset.id);
+  let media = mediasToDisplay.media;
+  document.querySelector(".lightbox__prev").dataset.id = mediasToDisplay.previous;
+  document.querySelector(".lightbox__next").dataset.id = mediasToDisplay.next;
   // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
   lightboxbg.style.display = "block";
   lightboxCnt.style.display = "block";
   
   // lightboxBtn.style.display = "flex";
-  if(elt.nodeName == "IMG"){
-    document.querySelector(".lightbox__container").innerHTML = `<img src="${elt.src}" style="width:100%">`;
+  if(media.image){
+    document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${media.image}" style="width:100%"><p>${media.title}</p>`;
     
   }else{
-    document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${elt.src}" style="width:100%" type="video/mp4"></video>`;
+    document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${media.title}" style="width:100%" type="video/mp4"></video>`;
   }
   // photographerTitle.innerHTML = `${photographer.title}`;
 }
@@ -205,80 +194,80 @@ function imageLightboxListener() {
 }
 
 // Navigation prochaine et précédente (souris et clavier)
-document.querySelector(".lightbox__next").addEventListener('click', () => goToNextSlide())
-document.querySelector(".lightbox__prev").addEventListener('click', () => goToPreviousSlide())
+document.querySelector(".lightbox__next").addEventListener('click', (e) => launchLightbox(e.target))
+document.querySelector(".lightbox__prev").addEventListener('click', (e) => launchLightbox(e.target))
 
 document.addEventListener('keydown', (e) => {
   const keyCode = e.key
   if (keyCode === 'ArrowRight') {
-    goToNextSlide()
+    document.querySelector(".lightbox__next").dataset.id = mediasToDisplay.next;
   } else if (keyCode === 'ArrowLeft') {
-    goToPreviousSlide()
+    document.querySelector(".lightbox__prev").dataset.id = mediasToDisplay.previous;
   } 
 })
 
-function goToNextSlide() {
-  const photographerFolder = getPhotographerFolder(photographer.name);
-  let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
-  let id =  document.querySelector(".lightbox__next").dataset.id;
-  console.log("mediasFromData", mediasFromData);
-  let previousId;
-  let nextId;
-  let mediaIndex = mediasFromData.findIndex(media => media.id == id);
-  if(mediaIndex == 0){
-    previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
-    nextId = mediasFromData[1].id;
-  }else if(mediaIndex == (mediasFromData.length - 1)){
-    previousId = mediasFromData[mediasFromData.length - 2].id;
-    nextId = mediasFromData[0].id;
-  }else{
-    previousId = mediasFromData[mediaIndex - 1].id;
-    nextId = mediasFromData[mediaIndex + 1].id;
-  }
-  document.querySelector(".lightbox__next").dataset.id = nextId;
-  document.querySelector(".lightbox__prev").dataset.id = previousId;
-  console.log(id, previousId, nextId);
-  // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
-  lightboxbg.style.display = "block";
-  lightboxCnt.style.display = "block";
-  // lightboxBtn.style.display = "flex";
-  if(mediasFromData[mediaIndex].image){
-    document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${mediasFromData[mediaIndex].image}" style="width:100%">`;
-  }else{
-    document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${mediasFromData[mediaIndex].video}" style="width:100%" type="video/mp4"></video>`;
-  }
-}
-function goToPreviousSlide() {
-  const photographerFolder = getPhotographerFolder(photographer.name);
-  let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
-  let id =  document.querySelector(".lightbox__prev").dataset.id;
-  console.log("mediasFromData", mediasFromData);
-  let previousId;
-  let nextId;
-  let mediaIndex = mediasFromData.findIndex(media => media.id == id);
-  if(mediaIndex == 0){
-    previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
-    nextId = mediasFromData[1].id;
-  }else if(mediaIndex == (mediasFromData.length - 1)){
-    previousId = mediasFromData[mediasFromData.length - 2].id;
-    nextId = mediasFromData[0].id;
-  }else{
-    previousId = mediasFromData[mediaIndex - 1].id;
-    nextId = mediasFromData[mediaIndex + 1].id;
-  }
-  document.querySelector(".lightbox__next").dataset.id = nextId;
-  document.querySelector(".lightbox__prev").dataset.id = previousId;
-  console.log(id, previousId, nextId);
-  // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
-  lightboxbg.style.display = "block";
-  lightboxCnt.style.display = "block";
-  // lightboxBtn.style.display = "flex";
-  if(mediasFromData[mediaIndex].image){
-    document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${mediasFromData[mediaIndex].image}" style="width:100%">`;
-  }else{
-    document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${mediasFromData[mediaIndex].video}" style="width:100%" type="video/mp4"></video>`;
-  }
-}
+// function goToNextSlide() {
+//   const photographerFolder = getPhotographerFolder(photographer.name);
+//   let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
+//   let id =  document.querySelector(".lightbox__next").dataset.id;
+//   console.log("mediasFromData", mediasFromData);
+//   let previousId;
+//   let nextId;
+//   let mediaIndex = mediasFromData.findIndex(media => media.id == id);
+//   if(mediaIndex == 0){
+//     previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
+//     nextId = mediasFromData[1].id;
+//   }else if(mediaIndex == (mediasFromData.length - 1)){
+//     previousId = mediasFromData[mediasFromData.length - 2].id;
+//     nextId = mediasFromData[0].id;
+//   }else{
+//     previousId = mediasFromData[mediaIndex - 1].id;
+//     nextId = mediasFromData[mediaIndex + 1].id;
+//   }
+//   document.querySelector(".lightbox__next").dataset.id = nextId;
+//   document.querySelector(".lightbox__prev").dataset.id = previousId;
+//   console.log(id, previousId, nextId);
+//   // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
+//   lightboxbg.style.display = "block";
+//   lightboxCnt.style.display = "block";
+//   // lightboxBtn.style.display = "flex";
+//   if(mediasFromData[mediaIndex].image){
+//     document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${mediasFromData[mediaIndex].image}" style="width:100%">`;
+//   }else{
+//     document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${mediasFromData[mediaIndex].video}" style="width:100%" type="video/mp4"></video>`;
+//   }
+// }
+// function goToPreviousSlide() {
+//   const photographerFolder = getPhotographerFolder(photographer.name);
+//   let baseUrl = `${getDomainFromUrl()}/FishEye_Photos/Sample_Photos/${photographerFolder}/`
+//   let id =  document.querySelector(".lightbox__prev").dataset.id;
+//   console.log("mediasFromData", mediasFromData);
+//   let previousId;
+//   let nextId;
+//   let mediaIndex = mediasFromData.findIndex(media => media.id == id);
+//   if(mediaIndex == 0){
+//     previousId = mediasFromData[mediasFromData.length - 1].id; // le dernier item
+//     nextId = mediasFromData[1].id;
+//   }else if(mediaIndex == (mediasFromData.length - 1)){
+//     previousId = mediasFromData[mediasFromData.length - 2].id;
+//     nextId = mediasFromData[0].id;
+//   }else{
+//     previousId = mediasFromData[mediaIndex - 1].id;
+//     nextId = mediasFromData[mediaIndex + 1].id;
+//   }
+//   document.querySelector(".lightbox__next").dataset.id = nextId;
+//   document.querySelector(".lightbox__prev").dataset.id = previousId;
+//   console.log(id, previousId, nextId);
+//   // const lightboxBtn = document.querySelectorAll(".photographs__pictures");
+//   lightboxbg.style.display = "block";
+//   lightboxCnt.style.display = "block";
+//   // lightboxBtn.style.display = "flex";
+//   if(mediasFromData[mediaIndex].image){
+//     document.querySelector(".lightbox__container").innerHTML = `<img src="${baseUrl}/${mediasFromData[mediaIndex].image}" style="width:100%">`;
+//   }else{
+//     document.querySelector(".lightbox__container").innerHTML = `<video  controls src="${baseUrl}/${mediasFromData[mediaIndex].video}" style="width:100%" type="video/mp4"></video>`;
+//   }
+// }
 
 const setNodeAttributes = (lastItem, currentItem) => {
   lastItem.style.display = 'none'
@@ -420,4 +409,23 @@ function filterMedias(selectedValue) {
   return orderedMedias;
 }
 
-
+function pickUpMediasToDisplay(id){
+  let previous;
+  let next;
+  let mediaIndex = mediasFromData.findIndex(media => media.id == id);
+  if(mediaIndex == 0){
+    previous = mediasFromData[mediasFromData.length - 1].id; // le dernier item
+    next = mediasFromData[1].id;
+  }else if(mediaIndex == (mediasFromData.length - 1)){
+    previous = mediasFromData[mediasFromData.length - 2].id;
+    next = mediasFromData[0].id;
+  }else{
+    previous = mediasFromData[mediaIndex - 1].id;
+    next = mediasFromData[mediaIndex + 1].id;
+  }
+  return{
+    previous,
+    media : mediasFromData[mediaIndex],
+    next
+  }
+}
