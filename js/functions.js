b402da544904012bb1c174b6d832bcc4eb618099
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-08-19 12:28:31 +0200

    button and alt changes

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 export function displayTags (tags) {
   let stringTemplate = ''
   tags.forEach(tag => {
-    stringTemplate += `<span class="tag" tabindex="0">#${tag}</span>`
+    stringTemplate += `<span class="tag" tabindex="0" role="button">#${tag}</span>`
   })
   return stringTemplate
 }
